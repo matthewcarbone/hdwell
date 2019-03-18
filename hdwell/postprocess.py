9af06404896fdec827d37f4c9163d8ceebec42c3
Matthew Carbone <x94carbone@gmail.com>
2019-03-18 16:24:40 -0400

    Updates to postprocess

--- hdwell/postprocess.py
+++ hdwell/postprocess.py
@@ -17,7 +17,7 @@ from cycler import cycler
 from itertools import product
 from collections import Counter
 
-from .aux import order_of_magnitude, hxw
+from .aux import order_of_magnitude, hxw, makedir_if_not_exist
 from .templates import PLOTTING_INFO_TEMPLATE, PLOTTING_PROTOCOL_MAP
 
 from . import logger  # noqa
@@ -497,6 +497,13 @@ def concatenator(data_path, prompt=True, s_by='beta'):
 
     for directory in all_dirs:
         run_path = os.path.join(data_path, directory)
+        makedir_if_not_exist(run_path, 'concat')
+
+        if prompt:
+            x_ = input("Concat: %s" % run_path)
+            if x_ != 'y':
+                continue
+
         param_path = os.path.join(run_path, 'params.csv')
         df = pd.read_csv(param_path, index_col=0)
 
@@ -519,7 +526,7 @@ def concatenator(data_path, prompt=True, s_by='beta'):
             N = len(sub_df)
             if len(df['nvec'].unique()) != 1:
                 raise RuntimeError("Non-unique nvec.")
-            print("len is ", N)
+
             # Initialize some matrices as None
             e_mat, r_mat, mem_c_mat, mem_b_mat = None, None, None, None
 
@@ -544,6 +551,15 @@ def concatenator(data_path, prompt=True, s_by='beta'):
                                          dtype=int)
                     psi_b_mat[0, :] = list(psi_b.values())
 
+                    # Load in the energy/min_radius grids:
+                    st = os.path.join(run_path, str_row, 'sample_e.pkl.pkl')
+                    e_grid = pickle.load(open(st, 'rb'))
+
+                    # Load in the memory grids:
+                    st = os.path.join(run_path, str_row, 'memory_basin.pkl')
+                    [__, mem_grid, domega] = pickle.load(open(st, 'rb'))
+                    first_str_row = str_row
+
                 else:
                     e_mat = np.concatenate((e_mat, e), axis=1)
                     r_mat = np.concatenate((r_mat, r), axis=1)
@@ -567,8 +583,53 @@ def concatenator(data_path, prompt=True, s_by='beta'):
                     psi_b_mat[index, :] = list(psi_b.values())
 
                 index += 1
-            print(e_mat.shape, r_mat.shape, mem_c_mat.shape, mem_b_mat.shape,
-                  psi_c_mat.shape, psi_b_mat.shape)
+
+            concat_loc_path = os.path.join(run_path, 'concat', first_str_row)
+            makedir_if_not_exist(concat_loc_path)
+
+            # Save the energy:
+            average_energy = np.mean(e_mat, axis=1)
+            std_energy = np.std(e_mat, axis=1)
+            e_path = os.path.join(concat_loc_path, 'energy.pkl')
+            pickle.dump([e_grid, average_energy, std_energy],
+                        open(e_path, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+
+            # Save the min radius:
+            average_min_r = np.mean(r_mat, axis=1)
+            std_min_r = np.std(r_mat, axis=1)
+            r_path = os.path.join(concat_loc_path, 'min_radius.pkl')
+            pickle.dump([e_grid, average_min_r, std_min_r],
+                        open(r_path, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+
+            # Memory functions
+            average_mem_b = np.mean(mem_b_mat, axis=1)
+            std_mem_b = np.std(mem_b_mat, axis=1)
+            mem_b_path = os.path.join(concat_loc_path, 'mem_b.pkl')
+            pickle.dump([mem_grid, average_mem_b, std_mem_b],
+                        open(mem_b_path, 'wb'),
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+            average_mem_c = np.mean(mem_c_mat, axis=1)
+            std_mem_c = np.std(mem_c_mat, axis=1)
+            mem_c_path = os.path.join(concat_loc_path, 'mem_c.pkl')
+            pickle.dump([mem_grid, average_mem_c, std_mem_c],
+                        open(mem_c_path, 'wb'),
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+            # Psi functions
+            average_psi_b = np.mean(psi_b_mat, axis=1)
+            std_psi_b = np.std(psi_b_mat, axis=1)
+            psi_b_path = os.path.join(concat_loc_path, 'psi_b.pkl')
+            pickle.dump([list(current_psi_b_keys), average_psi_b, std_psi_b],
+                        open(psi_b_path, 'wb'),
+                        protocol=pickle.HIGHEST_PROTOCOL)
+
+            average_psi_c = np.mean(psi_c_mat, axis=1)
+            std_psi_c = np.std(psi_c_mat, axis=1)
+            psi_c_path = os.path.join(concat_loc_path, 'psi_c.pkl')
+            pickle.dump([list(current_psi_c_keys), average_psi_c, std_psi_c],
+                        open(psi_c_path, 'wb'),
+                        protocol=pickle.HIGHEST_PROTOCOL)
 
 
 def plotting_tool(data_path, plotting_params, prompt=True):
